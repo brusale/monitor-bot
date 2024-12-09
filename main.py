071946de3172b75ab125c288c54cf8b924654aef
Alessandro Brusamolino <brusale11@gmail.com>
2024-12-09 21:20:20 +0100

    code format

--- main.py
+++ main.py
@@ -2,36 +2,38 @@ import logging
 import tomllib
 import argparse
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-		    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
-def create_parser(arg_list : list[str] | None = None):
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-c", "--config", help="TOML file for bot token and server url")
-	return parser
+
+def create_parser(arg_list: list[str] | None = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="TOML file for bot token and server url")
+    return parser
+
 
 def parse_config(config_file):
-	with open(config_file, "rb") as f:
-		config = tomllib.load(f)
+    with open(config_file, "rb") as f:
+        config = tomllib.load(f)
 
-	## get config for telegram bot
-	TOKEN = config["telegram-bot"]["API_TOKEN"]
-	CHAT_ID = config["telegram-bot"]["CHAT_ID"]
-	BOT_NAME = config["telegram-bot"]["NAME"]
+    ## get config for telegram bot
+    TOKEN = config["telegram-bot"]["API_TOKEN"]
+    CHAT_ID = config["telegram-bot"]["CHAT_ID"]
+    BOT_NAME = config["telegram-bot"]["NAME"]
 
-	## get config for target server
-	TARGET_URL = config["server"]["url"]
+    ## get config for target server
+    TARGET_URL = config["server"]["url"]
 
-	return TOKEN, CHAT_ID, BOT_NAME, TARGET_URL
+    return TOKEN, CHAT_ID, BOT_NAME, TARGET_URL
 
-def main(arg_list : list[str] | None = None):
-	args = create_parser(arg_list)
-	TARGET_URL, CHAT_ID, BOT_NAME, TARGET_URL = parse_config(args.config)
 
+def main(arg_list: list[str] | None = None):
+    args = create_parser(arg_list)
+    TARGET_URL, CHAT_ID, BOT_NAME, TARGET_URL = parse_config(args.config)
 
 
 if __name__ == "__main__":
-	main()
-	
+    main()
--- test/test_config_parsing.py
+++ test/test_config_parsing.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('./')
+
+sys.path.append("./")
 import unittest
 import tempfile
 import argparse
@@ -8,38 +9,38 @@ from unittest.mock import patch
 from main import create_parser, parse_config
 import toml
 
+
 class TestConfigParser(unittest.TestCase):
+    def test_create_parser(self):
+        parser = create_parser()
+        self.assertIsInstance(parser, argparse.ArgumentParser)
 
-	def test_create_parser(self):
-		parser = create_parser()
-		self.assertIsInstance(parser, argparse.ArgumentParser)
+    def test_parse_config(self):
+        with tempfile.TemporaryDirectory(dir=".") as tmp:
+            path = os.path.join(tmp)
+            print(path)
+            data = {
+                "telegram-bot": {
+                    "API_TOKEN": "token1234",
+                    "CHAT_ID": "chat1234",
+                    "NAME": "TestBot",
+                },
+                "server": {"url": "https://example.com"},
+            }
 
-	def test_parse_config(self):
-		with tempfile.TemporaryDirectory(dir=".") as tmp:
-			path = os.path.join(tmp)
-			print(path)
-			data = {
-				"telegram-bot": {
-					"API_TOKEN": "token1234",
-					"CHAT_ID": "chat1234",
-					"NAME": "TestBot"
-				},
-				"server": {
-					"url": "https://example.com"
-				}
-			
-			}
+            tmp_file = path + "/test_config.toml"
 
-			tmp_file = path+"/test_config.toml"
-	
-			with open(tmp_file, "w") as test_toml:
-				toml.dump(data, test_toml)
+            with open(tmp_file, "w") as test_toml:
+                toml.dump(data, test_toml)
 
-			result = parse_config(tmp_file)
-			self.assertEqual(result, ('token1234', 'chat1234', 'TestBot', 'https://example.com'))		
+            result = parse_config(tmp_file)
+            self.assertEqual(
+                result, ("token1234", "chat1234", "TestBot", "https://example.com")
+            )
 
-	def tearDown(self):
-		 patch.stopall()
+    def tearDown(self):
+        patch.stopall()
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
